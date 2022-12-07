16e0ba12cd442abe01c43cb5a4f1b7b537995c10
farinisgeorgios <farinisgeorgios@gmail.com>
2022-12-07 11:52:21 +0200

    Added  a logger into the main components

--- api/homegate/core/dataScraper.py
+++ api/homegate/core/dataScraper.py
@@ -10,6 +10,7 @@ from time import sleep
 import random
 import re
 import math
+import logging
 
 @dataclass
 class DataScraper:
@@ -22,10 +23,10 @@ class DataScraper:
     
     def __post_init__(self):
         self.base_page_url = f"""https://www.homegate.ch/{self.usage_type}/real-estate/matching-list?loc={'%2C'.join([f"geo-zipcode-{zipc}" for zipc in self.zipcodes])}"""
-        print(self.base_page_url)
         self.session = HTMLSession()
         self.df = pd.DataFrame()
         self.dataformatter = DataFormatter()
+        self.logger = logging.getLogger()
     
     
     def create_soup(self, url):
@@ -37,6 +38,7 @@ class DataScraper:
     def get_no_pages(self, initial_soup):
         results_found =  re.sub("[^0-9]", "", initial_soup.find_all('span', attrs={"class": lambda e: e.startswith('ResultsNumber_results') if e else False })[0].get_text())
         self.no_pages = math.ceil(float(results_found)/float(20))
+        self.logger.info(f"Pages Found: {self.no_pages}")
     
         
     def scrape_properties_data(self, properties):
@@ -75,31 +77,35 @@ class DataScraper:
                 new_row = pd.DataFrame(new_row_dict, index=[0])
                 self.df = pd.concat([new_row,self.df.loc[:]]).reset_index(drop=True)
             except ValidationError as e:
-                print(e)
+                self.logger.exception(e)
             finally:
                 pass
             
             
-        
     
     def get_pages_data(self):
         for page in range(self.no_pages):
             router_page_url = self.base_page_url + f"&ep={page+1}"
-            print(router_page_url)
+            self.logger.info(f"In Page: {router_page_url}")
             soup = self.create_soup(router_page_url)
         
             properties =  soup.find_all(attrs={"class": lambda e: e.startswith('ResultList_ListItem') if e else False })
-            print(len(properties))
+            self.logger.info(f"Real estates found in page: {len(properties)}")
+            self.logger.info("Procceding to scraping real estates.")
             self.scrape_properties_data(properties)
             
-            sleep(random.uniform(3.3,10.87))
-            print("sleeping...")
+            sleeping_time = random.uniform(3.3,10.87)
+            self.logger.info(f"Sleeping for {sleeping_time} seconds...")
+            sleep(sleeping_time)
+            
     
     
     def scrape_website(self):
         initial_soup = self.create_soup(self.base_page_url+"&ep=1")
         self.get_no_pages(initial_soup)
         self.get_pages_data()
+        
+        self.logger.info("Producing new data fields.")
         self.dataformatter.derive_new_fields(self.df)
         return self.df.sort_values(by=['euro/sqm'], ignore_index=True)
         
--- api/main.py
+++ api/main.py
@@ -1,7 +1,20 @@
 from homegate.HGManager import HGManager
+import logging
 
 
 if __name__ == "__main__":
+    
+    # Create `parent` logger
+    logger = logging.getLogger()
+    
+    # Set parent's level to INFO and assign a new handler
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s"))
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    
+    
+    logger.info("Running HomeGate Data Collector.")
     hgmanager = HGManager()
     df = hgmanager.scrape_website()
     print(df)
